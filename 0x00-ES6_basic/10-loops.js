018dc04c05299becfedf2231de6d36317f079aac
Malika <malikaoubilla@gmail.com>
2024-04-26 14:44:08 +0100

    10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const array2 = [];
   for (const value of array) {
-    array2.append(appendString + value);
+    array2.push(appendString + value);
   }
 
   return array2;
