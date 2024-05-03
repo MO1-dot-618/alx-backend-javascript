c7f9597db98e7a06da6eb4f46bc843d5e103c3f2
Malika <malikaoubilla@gmail.com>
2024-05-03 18:04:50 +0100

    4 fixxed again

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,15 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const gradedStudents = students.map((student) => {
     const gradey = newGrades.filter((gradeObject) => gradeObject.studentId === student.id);
-    if (gradey.length > 0) {
-      return { ...student, grade: parseInt(gradey[0].grade) };
-    } else {
-      return { ...student, grade: 'N/A' };
-    }
+    return {
+      ...student,
+      grade: gradey.length > 0 ? parseInt(gradey[0].grade, 10) : 'N/A',
+    };
   });
   return gradedStudents.filter((student) => student.location === city);
 }
+import getListStudents from "./0-get_list_students.js";
+
+console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
+
+console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
