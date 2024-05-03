574d954db35cc1f9344b518ad7c8bb3fcb186ac4
Malika <malikaoubilla@gmail.com>
2024-05-03 15:17:37 +0100

    2?

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-  if (!(students.isstudents(students)) || !students || !city) {
-    return [];
-  }
   return students.filter((student) => (student.location === city));
 }
