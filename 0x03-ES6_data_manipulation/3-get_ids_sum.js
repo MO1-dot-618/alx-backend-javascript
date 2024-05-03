632e6e31559396edde9f3ee5d32a55a1bbd5a4ad
Malika <malikaoubilla@gmail.com>
2024-05-03 15:26:48 +0100

    eslint

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 export default function getStudentIdsSum(array) {
   if (Array.isArray(array) && array) {
-    const array_ids = array.map(({ id }) => parseInt(id, 10)).filter(num => !isNaN(num));
-    const sum = array_ids.reduce((accumulator, currentValue) => (accumulator + currentValue), 0);
+    const arrayIds = array.map(({ id }) => parseInt(id, 10)).filter((num) => !Number.isNaN(num));
+    const sum = arrayIds.reduce((accumulator, currentValue) => (accumulator + currentValue), 0);
     return sum;
   }
   return 0;
