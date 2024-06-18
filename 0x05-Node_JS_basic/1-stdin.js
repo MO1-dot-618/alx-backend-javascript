9274ace050e83cd0cdc2374c29f4b61ca0e60f22
Malika <malikaoubilla@gmail.com>
2024-06-18 20:59:22 +0100

    task1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
+    console.log(`Your name is: ${name}`);
+    rl.close();
+});
+
+rl.on('close', () => {
+    console.log('This important software is now closing');
+    process.exit(0);
+});
